a7e523333dbde4c85ef125edfb291085f8447a77
Onur Tosyalioglu <onurtosyalioglu@gmail.com>
2018-11-14 14:14:44 +0300

    generic initialize script for git project

---
+++ git-init.sh
@@ -0,0 +1,32 @@
+git checkout -b development
+git push -u origin development
+git merge origin master
+
+git checkout -b production
+git push -u origin production
+git merge origin master
+
+git checkout -b test
+git push -u origin test
+git merge origin master
+
+git checkout -b feature
+git push -u origin feature
+git merge origin master
+
+git checkout -b database
+git push -u origin database
+git merge origin master
+
+git checkout -b hotfix
+git push -u origin hotfix
+git merge origin master
+
+git checkout -b frontend
+git push -u origin frontend
+git merge origin master
+
+git checkout -b devOps
+git push -u origin devOps
+git merge origin master
+
